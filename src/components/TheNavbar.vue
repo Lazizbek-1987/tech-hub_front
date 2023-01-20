5b7b4f0770d9f49392b4105ddbb216915af1da7a
Lazizbek <lazizabdullayev1987@gmail.com>
2023-01-20 22:39:43 +0500

    feat: done responsive navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,35 +1,52 @@
 <template>
     <div class="bg-white px-6 py-4 shadow-md shadow-blue-100 w-full fixed top-0">
-        <div class="flex space-x-6 items-center justify-between">
+        <div class="flex space-x-0 md:space-x-6 items-center justify-between">
             <div class="font-bold text-4xl cursor-pointer hover:scale-95 duration-500">
                 <span class="text-amber-500">Tech</span>
                 <span class="text-amber-600">Hub</span>
             </div>
-            <div>
-                <ul class="flex space-x-6 text-lg items-center">
-                    <li class="flex space-x-1 items-center">
-                        <span><HomeIcon class="w-6 h-6 text-blue-500"/></span>
-                        <span class="hover:cursor-pointer hover:text-blue-500 duration-500">Homepage</span>
-                    </li>
-                    <li class="flex space-x-1 items-center">
-                        <span><UsersIcon class="w-6 h-6 text-blue-500"/></span>
-                        <span class="hover:cursor-pointer hover:text-blue-500 duration-500">Connections</span>
-                    </li>
-                    <li class="flex space-x-1 items-center">
-                        <span><ChatBubbleLeftEllipsisIcon class="w-6 h-6 text-blue-500"/></span>
-                        <span class="hover:cursor-pointer hover:text-blue-500 duration-500">Messages</span>
-                    </li>
-                    <li class="flex space-x-1 items-center">
-                        <span><BellAlertIcon class="w-6 h-6 text-blue-500"/></span>
-                        <span class="hover:cursor-pointer hover:text-blue-500 duration-500">Notifications</span>
-                    </li>
-                    <li class="flex space-x-1 items-center">
-                        <span><Cog8ToothIcon class="w-6 h-6 text-blue-500"/></span>
-                        <span class="hover:cursor-pointer hover:text-blue-500 duration-500">Tools</span>
-                    </li>
-                </ul>
-            </div>
-            <div class="relative items-center">
+            <span
+                @click="menuOpen"
+                class="absolute block md:hidden w-6 h-6 items-center cursor-pointer right-6 top-5"
+            >
+            <XMarkIcon v-if="open"/>
+            <Bars3Icon v-else/>
+        </span>
+
+            <ul class="py-8 md:py-0 space-y-8 md:space-y-0 md:space-y-0 md:space-x-8 md:px-0 px-3 pb-10 pl-7 md:pl-0 md:pb-0 md:flex
+            md:items-center md:static absolute bg-gray-500 md:bg-white md:w-auto w-full top-14 duration-700 ease-in"
+                :class="[open ? 'left-0' : 'left-[-100%]']">
+                <li class="flex space-x-3 md:space-x-1 items-center">
+                    <span><HomeIcon class="w-8 h-8 md:w-6 md:h-6 text-amber-400 md:text-blue-500"/></span>
+                    <span
+                        class="text-white md:text-gray-700 hover:cursor-pointer text-xl md:text-lg hover:text-blue-500 duration-500"
+                    >Homepage
+                    </span>
+                </li>
+                <li class="flex space-x-3 md:space-x-1 items-center">
+                    <span><UsersIcon class="w-8 h-8 md:w-6 md:h-6 text-amber-400 md:text-blue-500"/></span>
+                    <span
+                        class="text-white md:text-gray-700 hover:cursor-pointer text-xl md:text-lg hover:text-blue-500 duration-500"
+                    >Connections
+                    </span>
+                </li>
+                <li class="flex space-x-3 md:space-x-1 items-center">
+                    <span><ChatBubbleLeftEllipsisIcon
+                        class="w-8 h-8 md:w-6 md:h-6 text-amber-400 md:text-blue-500"/></span>
+                    <span
+                        class="text-white md:text-gray-700 hover:cursor-pointer text-xl md:text-lg hover:text-blue-500 duration-500"
+                    >Messages
+                    </span>
+                </li>
+                <li class="flex space-x-3 md:space-x-1 items-center">
+                    <span><BellAlertIcon class="w-8 h-8 md:w-6 md:h-6 text-amber-400 md:text-blue-500"/></span>
+                    <span
+                        class="text-white md:text-gray-700 hover:cursor-pointer text-xl md:text-lg hover:text-blue-500 duration-500"
+                    >Notifications
+                    </span>
+                </li>
+            </ul>
+            <div class="relative items-center hidden lg:block">
                 <MagnifyingGlassIcon class="w-7 h-7 pt-2 pl-2 absolute"/>
                 <input
                     type="text"
@@ -44,12 +61,13 @@
 
 <script>
 import {
+    Bars3Icon,
     BellAlertIcon,
     ChatBubbleLeftEllipsisIcon,
-    Cog8ToothIcon,
     HomeIcon,
     MagnifyingGlassIcon,
-    UsersIcon
+    UsersIcon,
+    XMarkIcon
 } from '@heroicons/vue/24/outline'
 
 export default {
@@ -59,8 +77,19 @@ export default {
         UsersIcon,
         ChatBubbleLeftEllipsisIcon,
         BellAlertIcon,
-        Cog8ToothIcon,
-        MagnifyingGlassIcon
+        MagnifyingGlassIcon,
+        XMarkIcon,
+        Bars3Icon
+    },
+    data() {
+        return {
+            open: false
+        }
+    },
+    methods: {
+        menuOpen() {
+            this.open = !this.open
+        }
     }
 }
 </script>
